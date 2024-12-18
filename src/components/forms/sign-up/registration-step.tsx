31eeb22be127176fa311f28fbbaa804ab134dd94
sabarish <sabarish.cs8@gmail.com>
2024-12-18 16:07:48 +0530

    Add otp-form

--- src/components/forms/sign-up/registration-step.tsx
+++ src/components/forms/sign-up/registration-step.tsx
@@ -11,6 +11,10 @@ const DetailForm = dynamic(()=> import('./account-details-form'),{
     ssr:false,
     loading:Spinner,
 })
+const OTPForm = dynamic(() => import('./otp-form'),{
+    ssr:false,
+    loading:Spinner,
+})
 
 type Props = {}
 
@@ -41,6 +45,12 @@ const RegistrationFormStep = (props: Props) => {
         
         )
         case 3:
+            return (
+                <OTPForm 
+                onOTP={onOTP}
+                setOTP={setOnOTP}
+                />
+            )
     }
   return (
     <div>RegistrationFormStep</div>
