1b2f0be2b6a93c47b1d3fe7edd34f829463c24a3
sabarish <sabarish.cs8@gmail.com>
2024-12-24 20:26:02 +0530

    Add ->integrations

---
+++ src/components/integrations/IntegrationTrigger.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Modal from '../mondal'
+import { CloudIcon } from 'lucide-react'
+import { Card } from '../ui/card'
+import { Separator } from '../ui/separator'
+
+type Props = {
+    name: 'stripe'
+    logo: string
+    title: string
+    description: string
+    connections: {
+      [key in 'stripe']: boolean
+    }
+}
+
+const IntegrationTrigger = ({
+    name,
+    logo,
+    title,
+    description,
+    connections,
+  }: Props) => {
+  return (
+    <Modal
+    title={title}
+    type="Integration"
+    logo={logo}
+    description={description}
+    trigger={
+      <Card className="px-3 py-2 cursor-pointer flex gap-2">
+        <CloudIcon />
+        {connections[name] ? 'connected' : 'connect'}
+      </Card>
+    }
+  >
+     <Separator orientation="horizontal" />
+      {/* <IntegrationModalBody
+        connections={connections}
+        type={name}
+      /> */}
+    
+  </Modal>
+  )
+}
+
+export default IntegrationTrigger
